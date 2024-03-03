805f1e7f922201013124f068a97e6238f193ee3e
rbozek <rbozek@gmail.com>
2024-03-03 14:49:47 -0500

    define URL & view fxn for ABout

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def home(request):
   return HttpResponse('<h1>We\'re gonna get some reps!</h1>')
+
+def about(request):
+  return HttpResponse('<h1>This is the About page!</h1>')
+
