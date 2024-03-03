4c147ce7139a2167d3cec470c111d50eccfd6d4f
rbozek <rbozek@gmail.com>
2024-03-03 15:46:07 -0500

    render reps data in template - create UI in base.html, def route in urls, code index view in views (use temp Model from lesson), set up index page (& temp use CSS from lesson)

--- main_app/views.py
+++ main_app/views.py
@@ -3,9 +3,27 @@ from django.shortcuts import render
 #RB Step 1, for home view
 from django.http import HttpResponse
 
+
+class Rep:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, category, timespent, description, timestamp):
+    self.name = name
+    self.category = category
+    self.timespent = timespent
+    self.description = description
+    self.timestamp = timestamp
+
+reps = [
+  Rep('Practiced piano', 'Music', '30mins', 'Practiced scales & chords in 12 keys.', 'Tues 3 pm'),
+  Rep('Practiced guitar', 'Music', '45mins', 'Practiced songs for lessons.', 'Fri 10 am'),
+  Rep('Do some coding challenges', 'Coding', '1hr', 'Started working thru HackerRank challenges.', 'Mon 12 pm'),
+]
+
+
 def home(request):
   return HttpResponse('<h1>We\'re gonna get some reps!</h1>')
 
 def about(request):
   return render(request, 'about.html')
 
+def rep_index(request):
+  return render(request, 'reps/index.html', { 'reps': reps })
