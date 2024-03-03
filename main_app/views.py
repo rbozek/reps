a45b466ab88128296a9a3281e13d70ca4ede4900
rbozek <rbozek@gmail.com>
2024-03-03 14:47:14 -0500

    Defining View Functions - basic home function w httpresponse

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+#RB Step 1, for home view
+from django.http import HttpResponse
+
+def home(request):
+  return HttpResponse('<h1>We\'re gonna get some reps!</h1>')
