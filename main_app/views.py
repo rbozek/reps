47c90a7913486acbd73c0bfb0bf2a311e3d62984
rbozek <rbozek@gmail.com>
2024-03-04 12:01:59 -0500

    update views, remove temp model, import new actual model

--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
+from .models import Rep
 
 #RB Step 1, for home view - unused now that we re-defined
 # from django.http import HttpResponse
 
+#RB Step 2, temp store model here
+# class Rep:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, name, category, timespent, description, timestamp):
+#     self.name = name
+#     self.category = category
+#     self.timespent = timespent
+#     self.description = description
+#     self.timestamp = timestamp
 
-class Rep:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, category, timespent, description, timestamp):
-    self.name = name
-    self.category = category
-    self.timespent = timespent
-    self.description = description
-    self.timestamp = timestamp
-
-reps = [
-  Rep('Practiced piano', 'Music', '30mins', 'Practiced scales & chords in 12 keys.', 'Tues 3 pm'),
-  Rep('Practiced guitar', 'Music', '45mins', 'Practiced songs for lessons.', 'Fri 10 am'),
-  Rep('Do some coding challenges', 'Coding', '1hr', 'Started working thru HackerRank challenges.', 'Mon 12 pm'),
-]
+# reps = [
+#   Rep('Practiced piano', 'Music', '30mins', 'Practiced scales & chords in 12 keys.', 'Tues 3 pm'),
+#   Rep('Practiced guitar', 'Music', '45mins', 'Practiced songs for lessons.', 'Fri 10 am'),
+#   Rep('Do some coding challenges', 'Coding', '1hr', 'Started working thru HackerRank challenges.', 'Mon 12 pm'),
+# ]
 
 
 def home(request):
@@ -26,4 +27,5 @@ def about(request):
   return render(request, 'about.html')
 
 def rep_index(request):
+  reps = Rep.objects.all()
   return render(request, 'reps/index.html', { 'reps': reps })
