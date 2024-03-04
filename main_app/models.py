99470ce7b194e961dc530e1067f298358ca3abff
rbozek <rbozek@gmail.com>
2024-03-04 11:56:38 -0500

    created Rep model in models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Rep(models.Model):
+  name = models.CharField(max_length=100)
+  category = models.CharField(max_length=100)
+  timespent = models.IntegerField()
+  description = models.TextField(max_length=500)
+  created = models.DateTimeField(auto_now_add=True)
+  # created = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
+  updated = models.DateTimeField(auto_now=True)
+  updated = models.DateTimeField(auto_now=True, editable=False, null=False, blank=False)
+
+  def __str__(self):
+    return self.name
