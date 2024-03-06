8f2ba69f011026d72edbfe7889ed1cbf0a252ed3
rbozek <rbozek@gmail.com>
2024-03-06 15:43:36 -0500

    update Category model to allow blank field for color

--- main_app/models.py
+++ main_app/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
   name = models.CharField(max_length=50)
-  color = models.CharField(max_length=20)
+  color = models.CharField(max_length=25, null=True, blank=True)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
