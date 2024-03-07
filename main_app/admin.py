a28c662cb0d9c8fd05e3239b88d6aeec55d08f0e
rbozek <rbozek@gmail.com>
2024-03-07 18:23:53 -0500

    register Photo model with admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 # import your models here
-from .models import Rep, Category
+from .models import Rep, Category, Photo
 
 # Register your models here
 admin.site.register(Rep)
 admin.site.register(Category)
+admin.site.register(Photo)
